d51fdc905dd879e1ce5601882cc06755c6313f32
Boitumelo-code <124157854+Boitumelo-code@users.noreply.github.com>
2023-02-03 12:19:27 +0200

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sysDev2022;
+
+/**
+ *
+ * @author user
+ */
+public class Dog extends Animal{
+   String sound = "bark";
+   boolean hasWiskers = true;
+   boolean hasPaws = true;
+   String family = "Canidae";
+  public static void main(String[] args) 
+  {
+    
+      Dog dd = new Dog();
+      
+      System.out.println(dd.Teeth + " "+ dd.hasWiskers + " " + dd.haveTails);
+      
+      
+      
+      
+      
+  }
+           
+    
+    
+    
+}
