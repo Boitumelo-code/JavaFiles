318fabf2c4da314f3ff696e0319ce477ac3fa660
Boitumelo-code <124157854+Boitumelo-code@users.noreply.github.com>
2023-03-06 09:21:26 +0200

    Add files via upload

---
+++ Exercise.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sysDev2022;
+
+/**
+ *
+ * @author user
+ */
+public class Exercise {
+  public static void main(String[] args) {
+   
+      int age = 20;
+   if(age> 17 && age<22){   
+      
+      
+   System.out.println("Qualifiied");
+      
+      
+  }   
+      else{ 
+    System.out.println("Not Qualified");
+    
+}
