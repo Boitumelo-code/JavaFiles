8b26c5c4c7783b1f6898df3505dfedbe3c2447af
Boitumelo-code <124157854+Boitumelo-code@users.noreply.github.com>
2023-03-06 09:24:18 +0200

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sysDev2022;
+
+/**
+ *
+ * @author user
+ */
+public class Datatypes {
+   public static void main(String[] args) 
+   {
+    //Reference Data Types
+       String mynames = "Mary Thobani";//first way
+       String mynames1 = new String("James Tailor");//2nd way
+       
+       
+       
+       mynames.toUpperCase();
+       System.out.println(mynames.toUpperCase());
+        System.out.println(mynames1.toUpperCase());
+       
+       
+       
+   }
